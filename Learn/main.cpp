bd309d615ab50e5b69eb5334b37556a85301c2cd
hanshubin <hanshubin@xd.com>
2020-08-16 15:44:12 +0800

    C++ concurrency 2-4.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -4,56 +4,66 @@
 #include <ctime>
 #include <cmath>
 #include <thread>
+#include <string>
 
-class thread_guard
+void open_document_and_display_gui(std::string const& filename)
 {
-	std::thread& t;
-public:
-	//referenced parameter can not use constant perfixion.
-	explicit thread_guard(std::thread& _t) : t(_t) {}
 
-	~thread_guard()
-	{
-		if (t.joinable())
-		{
-			t.join();
-		}
-	}
-	thread_guard(thread_guard const&) = delete;
-	//thread_guard(thread_guard &&) = delete;
-	thread_guard& operator=(thread_guard const&) = delete;
-};
-
-void do_something(int& i)
-{
-	++i;
 }
 
-struct func
+bool done_editing()
 {
-	int& i;
+	return true;
+}
 
-	func(int& _i) : i(_i) {}
-
-	void operator()()
-	{
-		for (unsigned j = 0; j < 10000; j++)
-		{
-			do_something(i);
-		}
-	}
+enum command_type
+{
+	open_new_document
 };
 
-void do_something_in_current_thread()
-{}
+struct user_command
+{
+	command_type type;
+
+	user_command() : type(open_new_document) {}
+
+};
+
+user_command get_user_input()
+{
+	return user_command();
+}
+
+std::string get_filename_from_user()
+{
+	return "foo.doc";
+}
+
+void process_user_input(user_command const& cmd)
+{
+
+}
+
+void edit_document(std::string const& filename)
+{
+	open_document_and_display_gui(filename);
+	while (!done_editing())
+	{
+		user_command cmd = get_user_input();
+		if (cmd.type == open_new_document)
+		{
+			std::string const new_name = get_filename_from_user();
+			std::thread t(edit_document, new_name);
+			t.detach();
+		}
+		else
+		{
+			process_user_input(cmd);
+		}
+	}
+}
 
 int main()
 {
-	int some_local_state;
-	func my_func(some_local_state);
-	std::thread t(my_func);
-	thread_guard g(t);
-
-	do_something_in_current_thread();
-
+	edit_document("bar.doc");
 }
