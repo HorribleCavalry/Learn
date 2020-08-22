edb4cae51a9254cca9223ba95ccfef9761feed39
hanshubin <hanshubin@xd.com>
2020-08-22 23:02:13 +0800

    C++ exception learning list 15.13 P633 exception class.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
-#include <new>
-#include <cstdlib>//for exit();
+#include <stdexcept>
+#include <string>
+
+class Sales
+{
+public:
+	enum {MONTHS = 12};
+
+	class bad_index : public std::logic_error
+	{
+	private:
+		int bi;//bad idx value
+	public:
+		explicit bad_index(int ix, const std::string& s = "Index error in Sales object\n");
+		int bi_val() const { return bi; }
+		//I think this way of writing is equal to bottom
+		//virtual ~bad_index()noexcept {}
+		virtual ~bad_index()noexcept {}
+	};
+};
 
 struct Big
 {
