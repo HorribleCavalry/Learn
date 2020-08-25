5f0517269e664b659e7bb5ba537a9c31df5cd1ef
HorribleCavalry <HordrenHan@outlook.com>
2020-08-25 08:08:07 +0800

    C++ template advanced: template function.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -1,6 +1,26 @@
 ﻿#include <iostream>
 
+template<typename T>
+T Add(const T& a, const T& b)
+{
+	return a + b;
+}
+
+template<typename T>
+class vector
+{
+private:
+	T* element;
+public:
+	void push_back(T const&);
+	void clear();
+};
+template<typename T>
+inline void vector<T>::push_back(T const &)
+{
+}
 int main()
 {
+	
 	std::cout << "Hello world!";
 }
