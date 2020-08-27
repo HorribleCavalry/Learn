448676fe6c34bbba86b0fe75e79f259e2236a1cd
hanshubin <hanshubin@xd.com>
2020-08-27 23:29:41 +0800

    High level template specialization.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -3,6 +3,26 @@
 //Referenced: https://github.com/wuye9036/CppTemplateTutorial
 //Part 2.22
 
+template <typename T>
+class TypeToID
+{
+	static int const ID = 0x80000000;
+};
+
+template <>
+class TypeToID<float>
+{
+	static int const ID = 123;
+};
+
+template <typename T>
+class TypeToID<T*>
+{
+public:
+	typedef T		 SameAsT;
+	static int const ID = 0x80000000;
+};
+
 //You can even customize overloaded classes based on templates.
 template<typename T>
 class AddFloatOrMulInt
