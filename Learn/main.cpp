7cb92471725557f63700122a48bbbdbe50befebb
HorribleCavalry <HordrenHan@outlook.com>
2020-08-30 11:56:53 +0800

    Tried template vec2.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -1,53 +1,107 @@
 ﻿#include <iostream>
 #include <cassert>
 //Referenced: https://github.com/wuye9036/CppTemplateTutorial
-//Part 2.2 Remove Pointer.
+//Test vec.
 
 template<typename T>
-class TypeToId
+class vec2
 {
 public:
-	static const int id = 0;
+	T x,y;
+public:
+	vec2() : x(0), y(0) {}
+	vec2(const T& _x, const T& _y) : x(_x), y(_y) {}
+	vec2(const T& n) : x(n), y(n) {}
 };
 
-template<>
-class TypeToId<float>
+class vec2f : public vec2<float>
 {
 public:
-	static const int id = 1;
+	vec2f() : vec2<float>() {}
+	template<typename T>
+	vec2f(const T& _x, const T& _y) : vec2<float>(_x, _y) {}
+	template<typename T>
+	vec2f(const T& n) : vec2<float>(n) {}
+
+	template<typename T>
+	vec2f(const T& n) : vec2<float>(n.x, n.y) {}
+
+	//vec2f(const vec2i& v) : vec2<float>(n) {}
+
+
 };
 
+class vec2i : public vec2<int>
+{
+public:
+	vec2i() : vec2<int>() {}
+	vec2i(const int& _x, const int& _y) : vec2<int>(_x,_y){}
+	explicit vec2i(const vec2f& v)
+	{
+		x = static_cast<int>(v.x);
+		y = static_cast<int>(v.y);
+	}
+
+	const vec2i& operator=(const vec2i& v)
+	{
+		x = v.x;
+		y = v.y;
+	}
+};
+
+
+
+const vec2f& operator+(const vec2i& v0, const vec2f& v1)
+{
+	return vec2f(v0.x + v1.x, v1.y + v1.y);
+}
+const vec2f& operator+(const vec2f& v0, const vec2i& v1)
+{
+	return vec2f(v0.x + v1.x, v1.y + v1.y);
+}
 template<typename T>
-class TypeToId<T*>
+const vec2f& operator+(const T& n, const vec2f& v)
 {
-public:
-	typedef T	SameAsT;
-	static const int id = 2;
-};
-
+	return vec2f(n + v.x, n + v.y);
+}
 template<typename T>
-class RemovePointer
+const vec2f& operator+(const vec2f& v, const T& n)
 {
-
-};
-
-template<typename T>
-class RemovePointer<T*>
+	return vec2f(v.x + n, v.y + n);
+}
+const vec2f& operator+(const vec2i& v, const float& n)
 {
-public:
-	typedef typename RemovePointer<T>::TD TD;
-};
-
-template<typename T>
-class AddPointer
+	return vec2f(v.x + n, v.y + n);
+}
+const vec2f& operator+(const float& n, const vec2i& v)
 {
-public:
-	typedef T* TP;
-};
+	return vec2f(v.x + n, v.y + n);
+}
+
+const vec2i& operator+(const vec2i& v0, const vec2i& v1)
+{
+	return vec2i(v0.x + v1.x, v1.y + v1.y);
+}
+const vec2i& operator+(const int& n, const vec2i& v)
+{
+	return vec2i(v.x + n, v.y + n);
+}
+const vec2i& operator+(const vec2i& v, const int& n)
+{
+	return vec2i(v.x + n, v.y + n);
+}
+
 
 int main()
 {
-	RemovePointer<int**>::TD x;
-	AddPointer<int**>::TP ptt;
-	//auto testType = x;
+	vec2i i;
+	vec2f f;
+
+	const vec2i i;
+	const vec2f f;
+	
+	float in = 0;
+	float fn = 0.0f;
+
+	auto temp = (f = i);
 }
