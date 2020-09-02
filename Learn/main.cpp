f5fe74c57a9562ff742a86b86e1025c5ad54798b
hanshubin <hanshubin@xd.com>
2020-09-02 22:14:02 +0800

    Learned some template skill in math operation.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <cassert>
+#include <complex>
+#include <type_traits>
 //Referenced: https://github.com/wuye9036/CppTemplateTutorial
 //
 
@@ -47,6 +49,35 @@ const vec2<T> Add(const vec2<T>& v0, const vec2<T>& v1)
 {
 	return vec2<T>(v0.x, +v1.x, v0.y + v1.y);
 }
+template<typename T, bool isFloat = std::is_floating_point<T>::value, bool isInt = std::is_integral<T>::value>
+struct SafeDivide
+{
+	static void Do(const T&a, const T& b)
+	{
+		std::cout << "Called another!\n";
+	}
+};
+
+template<typename T>
+struct SafeDivide<T,true,false>
+{
+	static T Do(const T a, const T b)
+	{
+		std::cout << "Called float\n";
+		return a / b;
+	}
+};
+
+template<typename T>
+struct SafeDivide<T, false,true>
+{
+	static T Do(const T a, const T b)
+	{
+		std::cout << "Called int\n";
+		//assert(b != 0);
+		return a / b;
+	}
+};
 
 int main()
 {
@@ -54,6 +85,12 @@ int main()
 	Person<float> p1;
 	Person<float*> p2;
 	
+	SafeDivide<int>::Do(1, 2);
+	//SafeDivide<int>::Do(1, 0);
+	SafeDivide<float>::Do(1.0, 2.0);
+
+	SafeDivide<std::complex<float>>::Do({ 1.0f,2.0f }, { 1.0f,-2.0f });
+
 	//It can not work, I don't know why.
 	//Person<int, int> p2;
 }
