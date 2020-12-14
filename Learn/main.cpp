36b525bea655022ffcecdfa4a35aad2880114d30
hanshubin <hanshubin@xd.com>
2020-12-14 23:48:37 +0800

    First try std::set_new_handler.

--- Learn/main.cpp
+++ Learn/main.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
+#include <new>
+
+//This function will run when called std::set_new_handler(handler) and memory allocation failed.
+void handler()
+{
+	std::cout << "Memory allocation failed, terminating\n";
+	std::set_new_handler(nullptr);
+}
 
 int main()
 {
 	std::cout << "Hello world!";
+
+	std::set_new_handler(handler);
+
+	try
+	{
+		while (true)
+		{
+			new int[100000000ul];
+		}
+	}
+	catch (const std::bad_alloc& e)
+	{
+		std::cout << e.what();
+	}
+
 }
